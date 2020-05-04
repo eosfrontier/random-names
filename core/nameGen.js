fc9753ab9a33b32a56219ba053f181422f329f77
Thijs Boerma <thijs.boerma@sogeti.com>
2020-05-05 01:10:23 +0200

    implemented GET route for getting X amount of names from FACTION json namelist files, randomized.

--- core/nameGen.js
+++ core/nameGen.js
@@ -1,10 +1,11 @@
-const { DefaultAmount, Factions, MaxAmount } = require("../config");
-const fs = require('fs')
+const fs = require("fs")
+const { DefaultAmount, Factions, MaxAmount } = require("../config")
 
-const _checkFaction = (faction = '') => Factions.includes(faction.toLowerCase())
+// Validation rules & defaults for the HTTP Query
+const _checkFaction = (faction = "") => Factions.includes(faction.toLowerCase())
 const _enforceNumber = (input) => {
   const amount = parseInt(input)
-  return (amount && typeof amount === "number") ? amount : DefaultAmount
+  return amount && typeof amount === "number" ? amount : DefaultAmount
 }
 const _enforceLimit = (amount = DefaultAmount) => amount > MaxAmount ? MaxAmount : amount
 const _parseQuery = (query) => {
@@ -14,15 +15,35 @@ const _parseQuery = (query) => {
   }
 }
 
-const _loadFactionList = (factionName) => {
-  console.log(fs.existsSync(`./namelists/${factionName}.json`))
+// namelist private functions
+const _loadFactionList = (factionName) => JSON.parse(fs.readFileSync(`./namelists/${factionName}.json`))
+const _getRandomArrayItem = (array) => array[Math.floor(Math.random() * array.length)]
+
+// Create a single name based on the 'desiredOutput' field in the nameList JSON
+const _createNameFromList = (nameList) => {
+  let name = ''
+  nameList.desiredOutput.map((part) => {
+    name += _getRandomArrayItem(nameList[part])
+  })
+  return name
 }
 
-function testQuery(query = {}) {
+// create an array containing X names based on nameList
+const _createNameArray = (nameList, amount) => {
+  const nameArr = []
+  for (i = 0; i < amount; i++) {
+    nameArr[i] = _createNameFromList(nameList)
+  }
+  return nameArr
+}
+
+// public function that ties it all together
+function generateNames(query = {}) {
   const cleanQuery = _parseQuery(query)
-  _loadFactionList(cleanQuery.faction);
+  const nameList = _loadFactionList(cleanQuery.faction)
+  return _createNameArray(nameList, cleanQuery.amount)
 }
 
 module.exports = {
-  testQuery
+  generateNames,
 }
