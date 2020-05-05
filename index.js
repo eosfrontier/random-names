b3da58a5e04a36699c96b4b62125252f75707243
Thijs Boerma <thijs.boerma@sogeti.com>
2020-05-05 17:28:30 +0200

    cors/express fix test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
+const cors = require('cors')
+const express = require('express');
+const app = express()
+app.use(cors())
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+})
+const http = require("http").Server(app)
+
 let port = process.env.PORT || 3737;
 const { generateNames } = require('./core/nameGen')
 
