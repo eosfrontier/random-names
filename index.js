52d7638b46a59a545f303a0857ed9f7a5e93f764
Thijs Boerma <thijs.boerma@sogeti.com>
2020-05-05 17:40:18 +0200

    Shorten CORS() code chunk

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const cors = require('cors')
-const express = require('express');
-const app = express()
-app.use(cors())
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-	next()
-})
+const express = require('express')
+const app = (express()).use(cors())
 const http = require("http").Server(app)
 
 let port = process.env.PORT || 3737;
